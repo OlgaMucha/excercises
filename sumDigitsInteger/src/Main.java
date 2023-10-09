32a4d3f5fce99bf735adfd1dff08a4314895d68c
Olga Oost <=>
2023-10-09 17:32:52 +0200

    add tests Sum Digits Integer

--- sumDigitsInteger/src/Main.java
+++ sumDigitsInteger/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+
+            //testing
+            //test cases
+            int number1 = 123;
+            int number2 = 456;
+            int number3 = 0;
+            int number4 = 555;
+
+            //expected results
+            int expectedResult1 = 6;
+            int expectedResult2 = 15;
+            int expectedResult3 = 0;
+            int expectedResult4 = 15;
+
+            int count = 1;
+
+            //result test
+            HashMap<Integer, Integer> setResults = new HashMap<>();
+            setResults.put(number1, expectedResult1);
+            setResults.put(number2, expectedResult2);
+            setResults.put(number3, expectedResult3);
+            setResults.put(number4, expectedResult4);
+
+            for(int number: setResults.keySet()){
+                int result = setResults.get(number);
+                SumDigitsInteger sdi = new SumDigitsInteger(number);
+                if(sdi.calculateSumDigitsInteger(number) == result){
+                    System.out.println("test " + count + " passed");
+                } else{
+                    System.out.println("test " + count + " failed");
+                }
+                count++;
+            }
+
     }
 }
--- sumDigitsInteger/src/SumDigitsInteger.java
+++ sumDigitsInteger/src/SumDigitsInteger.java
@@ -1,26 +1,15 @@
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+//(Sum the digits in an integer) Write a method that computes the sum of the digits
+//in an integer. Use the following method header:
+//public static int sumDigits(long n)
+//For example, sumDigits(234) returns 9 (= 2 + 3 + 4).
 
 public class SumDigitsInteger {
+    public int number;
 
-    //(Sum the digits in an integer) Write a method that computes the sum of the digits
-    //in an integer. Use the following method header:
-    //public static int sumDigits(long n)
-    //For example, sumDigits(234) returns 9 (= 2 + 3 + 4). (Hint: Use the % operator
-    //to extract digits and the / operator to remove the extracted digit. For instance,
-    //to extract 4 from 234, use 234 % 10 (= 4). To remove 4 from 234, use
-    //234 / 10 (= 23). Use a loop to repeatedly extract and remove the digit until
-    //all the digits are extracted. Write a test program that prompts the user to enter an
-    //integer then displays the sum of all its digits.
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number: ");
-        int number = scanner.nextInt();
-        System.out.println();
-        System.out.println("Sum of digits of integer " + number + " equals " + calculateSumDigitsInteger(number));
+    public SumDigitsInteger(int number) {
+        this.number = number;
     }
 
     /**method name: calculateSumDigitsInteger
@@ -28,9 +17,9 @@ public class SumDigitsInteger {
      * @param number (int)
      * @return sumOfDigits (int) sum of digits of given number
      */
-    private static int calculateSumDigitsInteger(int number){
+    public int calculateSumDigitsInteger(int number){
         ArrayList<Integer> array = new ArrayList<>();
-        int sumOfDigits = 0;
+        int sumOfDigits;
         String numberAsString = String.valueOf(number);
         int numberOfDigits = numberAsString.length();
 
@@ -48,7 +37,7 @@ public class SumDigitsInteger {
      * @param arrayList (ArrayList<Integer>)
      * @return sum (int)
      */
-    private static int sumElementsOfArray(ArrayList<Integer> arrayList){
+    public int sumElementsOfArray(ArrayList<Integer> arrayList){
         int sum = 0;
         for (Integer integer : arrayList) {
             sum = sum + integer;
