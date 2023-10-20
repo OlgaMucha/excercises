feff15e05bf0d6d216db568fab902c952bff6230
Olga Oost <=>
2023-10-20 11:05:28 +0200

    add program replacing wod in file

---
+++ replaceInFile/src/main/java/org/example/ReplaceTextInFile.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.File;
+
+public class ReplaceTextInFile {
+
+    //create file if it doesn't exist
+
+
+
+
+
+    //searched word
+    //read file - find all occurrences of the word
+    //write file - replace all occurrences with new word
+
+
+
+}
