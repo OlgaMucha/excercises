4a7cbd9015e6283ebcceb7147105917d527a991e
Olga Oost <=>
2023-10-20 11:32:02 +0200

    refactoring program replace word in text file

--- replaceInFile/src/main/java/org/example/Main.java
+++ replaceInFile/src/main/java/org/example/Main.java
@@ -1,61 +1,43 @@
 package org.example;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    public static final String TEXT_IN_FILE = """
+
+            What is Lorem Ipsum?
+
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.\s
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \s
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \s
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. \s
+            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', \s
+            making it look like readable English.\s
+            Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.\s
+            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).""";
+
     public static void main(String[] args) throws java.io.IOException {
 
-        String textInFile = """
 
-                What is Lorem Ipsum?
-
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.\s
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \s
-                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \s
-                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. \s
-                The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', \s
-                making it look like readable English.\s
-                Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.\s
-                Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).""";
-
-
-        String wordToReplace = "has";
-        String replacingWord = "had";
+        String wordToReplace = "dummy";
+        String replacingWord = "DUMMY";
         String filePathOld = "oldFile.txt";
         String filePathNew = "newFile.txt";
 
-        createFile(textInFile, filePathOld);
-        searchAndReplaceStringInFile(wordToReplace, replacingWord, filePathOld, filePathNew);
+        ReplaceTextInFile replaceTextInFile = new ReplaceTextInFile(filePathOld, filePathNew, TEXT_IN_FILE, wordToReplace, replacingWord);
+        //creating file with given text
+        replaceTextInFile.createFile(filePathOld, TEXT_IN_FILE);
+
+        //creating new file as copy of the old one but with replaced given word with  new one
+        replaceTextInFile.searchAndReplaceStringAndSavingTextToNewFile(wordToReplace, replacingWord, filePathOld, filePathNew);
     }
 
-    private static void createFile(String textInFile, String filePath) throws FileNotFoundException {
-        java.io.File file = new java.io.File(filePath);
 
-        java.io.PrintWriter output = new java.io.PrintWriter(file);
 
-        output.println(textInFile);
 
-        output.close();
-    }
-
-    private static void searchAndReplaceStringInFile(String wordToSearch, String replacingWord, String filePath, String targetFilePath) throws FileNotFoundException {
-        java.io.File fileOld = new java.io.File(filePath);
-        java.io.File fileNew = new java.io.File(targetFilePath);
-        Scanner scanner = new Scanner(fileOld);
-        java.io.PrintWriter printWriter = new java.io.PrintWriter(fileNew);
-        while (scanner.hasNext()) {
-            String s1 = scanner.nextLine();
-            String s2 = s1.replace(wordToSearch, replacingWord);
-            printWriter.println(s2);
-        }
-
-        printWriter.close();
-    }
 }
 
--- replaceInFile/src/main/java/org/example/ReplaceTextInFile.java
+++ replaceInFile/src/main/java/org/example/ReplaceTextInFile.java
@@ -1,12 +1,48 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReplaceTextInFile {
+    public String oldFilePath;
+    public String newFilePath;
+    public String fileText;
+    public String oldWord;
+    public String newWord;
+
+    public ReplaceTextInFile(String oldFilePath, String newFilePath, String fileText, String oldWord, String newWord) {
+        this.oldFilePath = oldFilePath;
+        this.newFilePath = newFilePath;
+        this.fileText = fileText;
+        this.oldWord = oldWord;
+        this.newWord = newWord;
+    }
 
     //create file if it doesn't exist
+    public void createFile(String filePath, String fileText) throws FileNotFoundException {
+        java.io.File file = new java.io.File(filePath);
 
+        java.io.PrintWriter output = new java.io.PrintWriter(file);
 
+        output.println(fileText);
+
+        output.close();
+    }
+
+    public void searchAndReplaceStringAndSavingTextToNewFile(String oldWord, String newWord, String filePath, String targetFilePath) throws FileNotFoundException {
+        java.io.File fileOld = new java.io.File(filePath);
+        java.io.File fileNew = new java.io.File(targetFilePath);
+        Scanner scanner = new Scanner(fileOld);
+        java.io.PrintWriter printWriter = new java.io.PrintWriter(fileNew);
+        while (scanner.hasNext()) {
+            String s1 = scanner.nextLine();                         //old file as String
+            String s2 = s1.replace(oldWord, newWord);    //replacing strings
+            printWriter.println(s2);                                //new file as String
+        }
+
+        printWriter.close();
+    }
 
 
 
