d890ea193590f53b00dd7c9b6b8f568ecb14aa47
Olga Oost <=>
2023-11-01 20:14:34 +0100

    add shuffle array list unit test2

--- 043. ShuffleArrayList/ShuffleArrayList/src/test/java/org/example/ArrayListClassTest.java
+++ 043. ShuffleArrayList/ShuffleArrayList/src/test/java/org/example/ArrayListClassTest.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +11,9 @@ class ArrayListClassTest {
     @Test
     void test_shuffleArrayList_shuffledListHasThisSameElementsAndSize() {
         ArrayList<Integer> arrayBefore = new ArrayList<>(List.of(1, 2, 3));
-        ArrayListClass alc = new ArrayListClass(arrayBefore);
-        alc.shuffleArrayList();
-        ArrayList<Integer> arrayAfter = alc.getArrayListNumbers();
+        ArrayListClass cut = new ArrayListClass(arrayBefore);
+        cut.shuffleArrayList();
+        ArrayList<Integer> arrayAfter = cut.getArrayListNumbers();
 
         assertAll(
                 "Grouped Assertions",
@@ -27,4 +25,25 @@ class ArrayListClassTest {
 
     }
 
+    @Test
+    void test_shuffleArrayList_shuffledListHasThisSameElementsAndSize2() {
+        //given
+        ArrayList<Integer> arrayBefore = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        ArrayListClass cut = new ArrayListClass(arrayBefore);
+
+        //when
+        cut.shuffleArrayList();
+        ArrayList<Integer> arrayAfter = cut.getArrayListNumbers();
+
+        //then
+        assertAll(
+                "Grouped Assertions",
+                () -> assertEquals(arrayAfter.size(), arrayAfter.size()),
+                () -> assertTrue(arrayAfter.contains(1)),
+                () -> assertTrue(arrayAfter.contains(2)),
+                () -> assertTrue(arrayAfter.contains(3)),
+                () -> assertTrue(arrayAfter.contains(4)),
+                () -> assertTrue(arrayAfter.contains(5))
+        );
+    }
 }
