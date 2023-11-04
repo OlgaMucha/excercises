7f8044ac50a5eb14d7c19704e10495995f5cefe2
Olga Oost <=>
2023-11-04 16:44:10 +0100

    add unit test multiplication, division

--- 044. RationalNumberCalculator/RationalNumberCalculator/src/test/java/org/example/RationalNumberTest.java
+++ 044. RationalNumberCalculator/RationalNumberCalculator/src/test/java/org/example/RationalNumberTest.java
@@ -16,19 +16,6 @@ class RationalNumberTest {
         });
     }
 
-    @Test
-    public void constructor_byDivisionNumeratorOfDivisorCantBeNull(){
-            //Code under test
-            RationalNumber cut = new RationalNumber(1,10);
-            RationalNumber div = new RationalNumber(0, 5);
-
-            Assertions.assertThrows(IllegalArgumentException.class,() -> {
-
-                cut.divideRationals(div);
-        });
-    }
-
-
     //TEST ADDITION
     @Test
     public void addRationals_testForCorrectResultAddTwoPositiveRationals() {
@@ -152,6 +139,149 @@ class RationalNumberTest {
 
         //then
         Assertions.assertAll("Test addition",
+                ()->assertEquals(expected1,actual1),
+                ()->assertEquals(expected2,actual2),
+                ()->assertEquals(expected3,actual3)
+        );
+    }
+
+
+    //TEST MULTIPLICATION
+    @Test
+    public void addRationals_testForCorrectResultMultiplyTwoPositiveRationals() {
+        //given
+        RationalNumber cut = new RationalNumber(1,2);
+
+        RationalNumber rationalNumber2 = new RationalNumber(2,3);
+        RationalNumber rationalNumber3 = new RationalNumber(1,4);
+        RationalNumber rationalNumber4 = new RationalNumber(2,5);
+        RationalNumber rationalNumber5 = new RationalNumber(20,50);
+        RationalNumber rationalNumber6 = new RationalNumber(100,233);;
+        //when
+        // 1/2 * 2/3 = 2/6
+        RationalNumber actual1 = cut.multiplyRationals(rationalNumber2);
+        RationalNumber expected1 = new RationalNumber(2,6);
+        // 1/2 * 1/4 = 1/8
+        RationalNumber actual2 = cut.multiplyRationals(rationalNumber3);
+        RationalNumber expected2 = new RationalNumber(1,8);
+        // 1/2 * 2/5 = 2/10
+        RationalNumber actual3 = cut.multiplyRationals(rationalNumber4);
+        RationalNumber expected3 = new RationalNumber(2,10);
+        // 1/2 * 20/50 = 20/100
+        RationalNumber actual4 = cut.multiplyRationals(rationalNumber5);
+        RationalNumber expected4 = new RationalNumber(20,100);
+        //  1/2 * 100/233 = 100/466
+        RationalNumber actual5 = cut.multiplyRationals(rationalNumber6);
+        RationalNumber expected5 = new RationalNumber(100,466);
+
+        //then
+        Assertions.assertAll("Test subtraction",
+                ()->assertEquals(expected1,actual1),
+                ()->assertEquals(expected2,actual2),
+                ()->assertEquals(expected3,actual3),
+                ()->assertEquals(expected4,actual4),
+                ()->assertEquals(expected5,actual5)
+        );
+    }
+
+    @Test
+    public void addRationals_testForCorrectResultMultiplyWithZeroRational() {
+        //given
+        RationalNumber cut = new RationalNumber(0,2);
+
+        RationalNumber rationalNumber2 = new RationalNumber(0,3);
+        RationalNumber rationalNumber3 = new RationalNumber(1,2);
+        RationalNumber rationalNumber4 = new RationalNumber(1,-2);
+        // 0/2 * 0/3 = 0/6
+        RationalNumber actual1 = cut.multiplyRationals(rationalNumber2);
+        RationalNumber expected1 = new RationalNumber(0,6);
+        // 0/2 * 1/2 = 0/4
+        RationalNumber actual2 = cut.multiplyRationals(rationalNumber3);
+        RationalNumber expected2 = new RationalNumber(0,4);
+        // 0/2 * 1/-2 = 0/-4
+        RationalNumber actual3 = cut.multiplyRationals(rationalNumber4);
+        RationalNumber expected3 = new RationalNumber(0,-4);
+
+        //then
+        Assertions.assertAll("Test addition",
+                ()->assertEquals(expected1,actual1),
+                ()->assertEquals(expected2,actual2),
+                ()->assertEquals(expected3,actual3)
+        );
+    }
+
+
+    //TEST DIVISION
+
+    @Test
+    public void constructor_byDivisionNumeratorOfDivisorCantBeNull(){
+        //Code under test
+        RationalNumber cut = new RationalNumber(1,10);
+        RationalNumber div = new RationalNumber(0, 5);
+
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+
+            cut.divideRationals(div);
+        });
+    }
+
+    @Test
+    public void addRationals_testForCorrectResultDivideTwoPositiveRationals() {
+        //given
+        RationalNumber cut = new RationalNumber(1,2);
+
+        RationalNumber rationalNumber2 = new RationalNumber(2,3);
+        RationalNumber rationalNumber3 = new RationalNumber(1,4);
+        RationalNumber rationalNumber4 = new RationalNumber(2,5);
+        RationalNumber rationalNumber5 = new RationalNumber(20,50);
+        RationalNumber rationalNumber6 = new RationalNumber(100,233);;
+        //when
+        // 1/2 / 2/3 = 3/4
+        RationalNumber actual1 = cut.divideRationals(rationalNumber2);
+        RationalNumber expected1 = new RationalNumber(3,4);
+        // 1/2 / 1/4 = 4/2
+        RationalNumber actual2 = cut.divideRationals(rationalNumber3);
+        RationalNumber expected2 = new RationalNumber(4,2);
+        // 1/2 / 2/5 = 5/4
+        RationalNumber actual3 = cut.divideRationals(rationalNumber4);
+        RationalNumber expected3 = new RationalNumber(5,4);
+        // 1/2 / 20/50 = 50/40
+        RationalNumber actual4 = cut.divideRationals(rationalNumber5);
+        RationalNumber expected4 = new RationalNumber(50,40);
+        //  1/2 / 100/233 = 233/200
+        RationalNumber actual5 = cut.divideRationals(rationalNumber6);
+        RationalNumber expected5 = new RationalNumber(233,200);
+
+        //then
+        Assertions.assertAll("Test division",
+                ()->assertEquals(expected1,actual1),
+                ()->assertEquals(expected2,actual2),
+                ()->assertEquals(expected3,actual3),
+                ()->assertEquals(expected4,actual4),
+                ()->assertEquals(expected5,actual5)
+        );
+    }
+
+    @Test
+    public void addRationals_testForCorrectResultDivideWithZeroRational() {
+        //given
+        RationalNumber cut = new RationalNumber(0,2);
+
+        RationalNumber rationalNumber2 = new RationalNumber(5,3);
+        RationalNumber rationalNumber3 = new RationalNumber(1,2);
+        RationalNumber rationalNumber4 = new RationalNumber(1,-2);
+        // 0/2 * 5/3 = 0/6
+        RationalNumber actual1 = cut.divideRationals(rationalNumber2);
+        RationalNumber expected1 = new RationalNumber(0,10);
+        // 0/2 * 1/2 = 0/4
+        RationalNumber actual2 = cut.divideRationals(rationalNumber3);
+        RationalNumber expected2 = new RationalNumber(0,2);
+        // 0/2 * 1/-2 = 0/-4
+        RationalNumber actual3 = cut.divideRationals(rationalNumber4);
+        RationalNumber expected3 = new RationalNumber(0,2);
+
+        //then
+        Assertions.assertAll("Test division",
                 ()->assertEquals(expected1,actual1),
                 ()->assertEquals(expected2,actual2),
                 ()->assertEquals(expected3,actual3)
