fb96f8d5a7b5b3bcac254d140e6bd9c59d37cfc0
Olga Oost <=>
2023-11-20 21:39:56 +0100

    add unit tests for method sortArrayList

---
+++ 052. Sort ArrayList/SortArrayList/src/test/java/org/example/ArrayListIntTest.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListIntTest {
+
+    @Test
+    void test_sortArrayList_empty() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(List.of());
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_element_1() {
+        ArrayList<Integer> arrayList = new ArrayList<>(List.of(1));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(List.of(1));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_elements_2_1() {
+        ArrayList<Integer> arrayList = new ArrayList<>(List.of(2,1));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(List.of(1,2));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_elements_2_minus1() {
+        ArrayList<Integer> arrayList = new ArrayList<>(List.of(2,-1));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(List.of(-1,2));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_elements_10_9_2_1_7() {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(10,9,2,1,7));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(Arrays.asList(1,2,7,9,10));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_elements_1_2_3() {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,2,3));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(Arrays.asList(1,2,3));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+
+    @Test
+    void test_sortArrayList_elements_10_2_10() {
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(10,2,10));
+        ArrayListInt cut = new ArrayListInt();
+        cut.setArrayList(arrayList);
+        ArrayList<Integer> sortedArrayActual = cut.sortArrayList();
+        ArrayList<Integer> sortedArrayExpected = new ArrayList<>(Arrays.asList(2,10,10));
+        Assertions.assertEquals(sortedArrayExpected,sortedArrayExpected);
+    }
+}
