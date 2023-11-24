73c9c85e603eb19e06d6494bb284b919c1fbd7ea
Olga Oost <=>
2023-11-24 18:56:00 +0100

    fixed bug

--- 053. InputMismatchException/InputMismachException/src/main/java/org/example/Main.java
+++ 053. InputMismatchException/InputMismachException/src/main/java/org/example/Main.java
@@ -2,33 +2,40 @@ package org.example;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        boolean inputCorrect = false;
+        boolean inputCorrect = true;
         int number1 = 0;
         int number2 = 0;
-        do {
 
+        do {
             try {
                 System.out.println("Enter 2 integers");
 
+                // Read the first integer
                 number1 = scanner.nextInt();
+
+                // Read the second integer
                 number2 = scanner.nextInt();
+
+
                 inputCorrect = true;
+            } catch (InputMismatchException ex) {
+                System.out.println("Input incorrect");
+                inputCorrect = false;
+
+                // Consume the invalid input
+                scanner.nextLine();
             }
-            catch (InputMismatchException ex){
-                System.out.println("input incorrect");
-            }
-        }
-        while(!inputCorrect);
+        } while (!inputCorrect);
 
         System.out.println("You entered integers " + number1 + " and " + number2);
-        }
 
-
+        // Close the scanner to release resources
+        scanner.close();
     }
+}
