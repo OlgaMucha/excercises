2a32f7d642a1c10d64bcdf7a1cef27583a1d89cc
Olga Oost <=>
2023-11-25 21:31:32 +0100

    054. methods to remove or replace text

--- 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/Main.java
+++ 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/Main.java
@@ -5,7 +5,28 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         TextFile textFile = new TextFile();
+
+        String text = """
+                De derde herfststorm van dit jaar is een feit.\s
+                 In IJmuiden werd vrijdag tussen 16.00 en 17.00 uur windkracht 9 gemeten,\s
+                 waarmee officieel sprake is van een storm, meldt Weeronline. Het is in totaal de vijfde storm van dit jaar.
+                Deze herfst stormde het eerder op 2 november en 19 september.\s
+                 Het is volgens Weeronline voor het eerst sinds 2017 dat het drie keer in de herfst stormt.""";
+
+        java.io.File file = new java.io.File("newFile.txt");
+
+        textFile.setText(text);
+        textFile.setFile(file);
+        //replace text
+        //textFile.replaceTextWithText("IJmuiden","Amsterdam");
+        //textFile.fillInFile();
+
+        //remove text
+        textFile.removeText("online");
         textFile.fillInFile();
 
+
     }
+
+
 }
--- 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/TextFile.java
+++ 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/TextFile.java
@@ -6,32 +6,44 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TextFile {
-    private java.io.File file = new java.io.File("newFile.txt");
-    private String text = "De derde herfststorm van dit jaar is een feit. " +
-            "\n" +
-            " In IJmuiden werd vrijdag tussen 16.00 en 17.00 uur windkracht 9 gemeten, " +
-            "\n" +
-            " waarmee officieel sprake is van een storm, meldt Weeronline. Het is in totaal de vijfde storm van dit jaar." +
-            "\n" +
-            "Deze herfst stormde het eerder op 2 november en 19 september. " +
-            "\n" +
-            " Het is volgens Weeronline voor het eerst sinds 2017 dat het drie keer in de herfst stormt.";
+    private java.io.File file;
+    private String text;
     //fill in file
 
     public void fillInFile() throws FileNotFoundException {
-        this.text = this.text.replace("is" , "was");
-
         PrintWriter printWriter = new PrintWriter(this.file);
         printWriter.println(this.text);
         printWriter.close();
     }
 
-    public void removeFromFileText(String text) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        }
+    //region Getters & Setters
+    public File getFile() {
+        return file;
+    }
 
+    public void setFile(File file) {
+        this.file = file;
+    }
 
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 
     public TextFile() {
     }
+
+    //endregion
+
+    public void replaceTextWithText(String oldText, String newText){
+        this.text = this.text.replace(oldText , newText);
+    }
+
+    public void removeText(String textToRemove){
+        replaceTextWithText(textToRemove,"");
+    }
+
 }
