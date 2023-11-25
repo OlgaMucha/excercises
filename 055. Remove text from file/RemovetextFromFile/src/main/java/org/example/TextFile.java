aed07a355cc6d7be41bdefb98a8ffb790f6f7a58
Olga Oost <=>
2023-11-25 21:07:58 +0100

    054.replace is with was

--- 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/TextFile.java
+++ 055. Remove text from file/RemovetextFromFile/src/main/java/org/example/TextFile.java
@@ -7,13 +7,22 @@ import java.util.Scanner;
 
 public class TextFile {
     private java.io.File file = new java.io.File("newFile.txt");
+    private String text = "De derde herfststorm van dit jaar is een feit. " +
+            "\n" +
+            " In IJmuiden werd vrijdag tussen 16.00 en 17.00 uur windkracht 9 gemeten, " +
+            "\n" +
+            " waarmee officieel sprake is van een storm, meldt Weeronline. Het is in totaal de vijfde storm van dit jaar." +
+            "\n" +
+            "Deze herfst stormde het eerder op 2 november en 19 september. " +
+            "\n" +
+            " Het is volgens Weeronline voor het eerst sinds 2017 dat het drie keer in de herfst stormt.";
     //fill in file
+
     public void fillInFile() throws FileNotFoundException {
+        this.text = this.text.replace("is" , "was");
 
         PrintWriter printWriter = new PrintWriter(this.file);
-        printWriter.println("De derde herfststorm van dit jaar is een feit. In IJmuiden werd vrijdag tussen 16.00 en 17.00 uur windkracht 9 gemeten, waarmee officieel sprake is van een storm, meldt Weeronline. Het is in totaal de vijfde storm van dit jaar.\n" +
-                "\n" +
-                "Deze herfst stormde het eerder op 2 november en 19 september. Het is volgens Weeronline voor het eerst sinds 2017 dat het drie keer in de herfst stormt.");
+        printWriter.println(this.text);
         printWriter.close();
     }
 
