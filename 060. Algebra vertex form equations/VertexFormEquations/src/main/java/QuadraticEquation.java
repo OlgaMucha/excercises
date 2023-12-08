04a7382e45f4bf00fb9409218731d9cfc0d10c59
OlgaMucha <olga.mucha@gmail.com>
2023-12-08 20:00:22 +0100

    060. class QuadraticEquation

---
+++ 060. Algebra vertex form equations/VertexFormEquations/src/main/java/QuadraticEquation.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Array;
+
+public class QuadraticEquation {
+
+    private double a;
+    private double b;
+    private double c;
+
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public Double[] changeToVertexForm(){
+
+        double h = -b/(2 * a);
+        double k = (4*a*b - b*b)/(4*a);
+        return new Double[]{h, k};
+    }
+}
