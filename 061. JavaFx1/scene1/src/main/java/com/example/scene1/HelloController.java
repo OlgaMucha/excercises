3260d11dcd4ceaea86dddef7917b96e82976333e
OlgaMucha <olga.mucha@gmail.com>
2023-12-26 13:32:33 +0100

    061 project setup

---
+++ 061. JavaFx1/scene1/src/main/java/com/example/scene1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.scene1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Hi Olga!");
+    }
+}
